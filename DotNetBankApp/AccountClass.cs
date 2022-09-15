4d3ba257513b39ea4841c6278d71217bfedadbbd
Natasha <natasha6.lee@gmail.com>
2022-09-15 17:41:52 +1000

    save progress

---
+++ DotNetBankApp/AccountClass.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetBankApp
+{
+    class AccountClass
+    {
+
+        public static int menuOption;
+
+        string firstName;
+        string lastName;
+        string address;
+        string phone;
+        string email;
+
+
+
+
+
+
+
+        public static void MainMenu() {
+
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("        Welcome to Simple Banking         ");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine(" 1: Create a new account                  ");
+            Console.WriteLine(" 2: Search for an account                 ");
+            Console.WriteLine(" 3: Deposit                               ");
+            Console.WriteLine(" 4: Withdraw                              ");
+            Console.WriteLine(" 5: A/C Statement                         ");
+            Console.WriteLine(" 6: Delete Account                        ");
+            Console.WriteLine(" 7: Exit                                  ");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine(" Enter a number 1-7:                      ");
+            Console.WriteLine("------------------------------------------");
+
+            Console.SetCursorPosition(21, 11);
+            validateSelection();
+
+
+
+
+        }
+
+        static void validateSelection(){
+
+            //this tests to see if the input is an integer, and then makes sure its actually between 1-7
+            //also gives user an error message 
+            while (!int.TryParse(Console.ReadLine(), out menuOption)) {
+                Console.SetCursorPosition(2, 13);
+                Console.WriteLine("Invalid input, press any key to try again.");
+                Console.ReadKey();
+                Console.Clear();
+                MainMenu();
+            }
+
+            if (menuOption > 0 && menuOption < 8) {
+                selector();
+
+            } else {
+
+                Console.SetCursorPosition(2, 13);
+                Console.WriteLine("Invalid input, press any key to try again.");
+                Console.ReadKey();
+                Console.Clear();
+                MainMenu();
+            }
+            
+        }
+
+        static void selector() {
+
+            switch (menuOption){
+
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+
+
+            }
+
+
+            Console.SetCursorPosition(2, 13);
+
+
+        
+        
+        }
+    }
+}
--- DotNetBankApp/Program.cs
+++ DotNetBankApp/Program.cs
@@ -7,13 +7,39 @@ namespace DotNetBankApp
     {
         public static string unInput;
         public static string passInput;
-        //when you log out remeber to set it back to false!
         public static Boolean correctDeet = false;
 
 
+        static void enterDetails()
+        {
 
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("        Welcome to Simple Banking         ");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("                                          ");
+            Console.WriteLine("                Login here:               ");
+            Console.WriteLine("                                          ");
+            Console.WriteLine("       Username:                          ");
+            Console.WriteLine("       Password:                          ");
+            Console.WriteLine("                                          ");
+            Console.WriteLine("------------------------------------------");
+
+
+            Console.SetCursorPosition(18, 6);
+            unInput = Console.ReadLine();
+            Console.SetCursorPosition(18, 7);
+            passInput = Console.ReadLine();
+            Console.SetCursorPosition(2, 10);
+
+            Login();
+
+
+
+        }
 
         static void Login() {
+
+            // read through the usernames and passwords in the text file. if credentials correct let the user log in, and invalid ones let them try again. 
 
             try
             {
@@ -23,8 +49,6 @@ namespace DotNetBankApp
                 {
                     string[] splits = set.Split('|');
 
-                    //tester dw
-                    //Console.WriteLine("{0}{1}", splits[0], splits[1]);
 
                     if (splits[0] == unInput && splits[1] == passInput)
                     {
@@ -32,29 +56,32 @@ namespace DotNetBankApp
                         
                     }
 
-                    else {
-
-                       //this is probably not the best way to do it?
-                    
-                    }
-
-
                 }
 
                 if (correctDeet)
                 {
-                    Console.WriteLine("Welcome {0}!", unInput);
+                    Console.WriteLine("Welcome {0}! Press any button to continue!", unInput);
+                    Console.ReadKey();
+                    Console.Clear();
+                    AccountClass.MainMenu();
+
+
 
                 }
                 else {
-                    Console.WriteLine("Invalid details, please try again");
+                    Console.WriteLine("\nInvalid details, press any key to try again");
+                    Console.ReadKey();
+                    Console.Clear();
+                    enterDetails();
 
+                    
 
                 }
 
 
             }
 
+            //cant find file error
             catch(FileNotFoundException e) {
                 Console.WriteLine(e.Message);
                 Console.WriteLine("Please press any key to exit application. Contact administrator for support.");
@@ -68,26 +95,7 @@ namespace DotNetBankApp
 
             //login screen 
 
-            Console.WriteLine("|------------------------------------------|");
-            Console.WriteLine("|        Welcome to Simple Banking         |");
-            Console.WriteLine("|------------------------------------------|");
-            Console.WriteLine("|                                          |");
-            Console.WriteLine("|                Login here:               |");
-            Console.WriteLine("|                                          |");
-            Console.WriteLine("|       Username:                          |");
-            Console.WriteLine("|       Password:                          |");
-            Console.WriteLine("|                                          |");
-            Console.WriteLine("|------------------------------------------|");
-
-            Console.SetCursorPosition(18, 6);
-            unInput = Console.ReadLine();
-            Console.SetCursorPosition(18, 7);
-            passInput = Console.ReadLine();
-            Console.SetCursorPosition(3, 8);
-
-            Login();
-            
-
+            enterDetails();
 
 
         }
